c2af6ee535d11b38762fa58d354125845e4c2275
Matthias Rolke <mr.amtrack@gmail.com>
2025-08-19 14:21:08 +0200

    wip

--- src/commands/metadata-coverage/check.ts
+++ src/commands/metadata-coverage/check.ts
@@ -3,28 +3,32 @@ import { ComponentSetBuilder } from "@salesforce/source-deploy-retrieve";
 import { readFileSync } from "node:fs";
 import { dirname, join } from "node:path";
 
-export type OrgHelloResult = {};
+export type MetadataCoverageResult = {
+  success: boolean;
+  outputString: string;
+};
 
-export class OrgHelloCommand extends SfCommand<OrgHelloResult> {
-  public static readonly summary = "print a greeting and your org IDs";
+export class MetadataCoverageCheck extends SfCommand<MetadataCoverageResult> {
+  public static readonly summary =
+    "check the Metadata Coverage for the given source";
   public static readonly examples = [
     "<%= config.bin %> <%= command.id %> --target-org myOrg@example.com",
-    "<%= config.bin %> <%= command.id %> --name myname --target-org myOrg@example.com",
   ];
 
   public static readonly requiresProject = true;
 
   public static readonly flags = {
-    name: Flags.string({
-      char: "n",
-      summary: "name to print",
-      default: "world",
-    }),
+    "api-version": Flags.orgApiVersion(),
   };
 
-  public async run(): Promise<OrgHelloResult> {
-    // const { flags } = await this.parse(OrgHelloCommand);
+  public async run(): Promise<MetadataCoverageResult> {
+    const { flags } = await this.parse(MetadataCoverageCheck);
+    console.error(flags["api-version"]);
     const apiVersion = "62.0";
+    const sourceApiVersion = this.project
+      ?.getSfProjectJson()
+      .get("sourceApiVersion");
+    console.error({ sourceApiVersion });
     const apiVersionMajor = apiVersion.split(".")[0];
     // const reportResult = await fetch(
     //   `https://dx-extended-coverage.my.salesforce-sites.com/services/apexrest/report?version=${apiVersionMajor}`
@@ -66,12 +70,21 @@ export class OrgHelloCommand extends SfCommand<OrgHelloResult> {
       sourcepath: sourcePaths,
     });
     const objects = await componentSet.getObject();
+    let success = true;
     for (const mdType of objects.Package.types) {
       const coverage = report.types[mdType.name];
       if (requiredChannels.some((channel) => !coverage?.channels[channel])) {
+        success = false;
         console.error(mdType.name, mdType.members, coverage?.channels);
       }
     }
-    return {};
+    const result: MetadataCoverageResult = {
+      success,
+      outputString: success
+        ? "Successfully checked Metadata Coverage Report."
+        : "Some metadata types are not covered.",
+    };
+    this.log(result.outputString);
+    return result;
   }
 }
---
+++ test/commands/metadata-coverage/check.test.ts
@@ -0,0 +1,33 @@
+import { TestContext } from "@salesforce/core/testSetup";
+import { expect } from "chai";
+import { MetadataCoverageCheck } from "../../../src/commands/metadata-coverage/check.js";
+
+describe("metadata-coverage check", () => {
+  const $$ = new TestContext();
+
+  beforeEach(async () => {
+    $$.setConfigStubContents("SfProjectJson", {
+      contents: {
+        packageDirectories: [
+          {
+            path: "force-app",
+            default: true,
+          },
+        ],
+        sourceApiVersion: "64.0",
+      },
+    });
+    // $$.inProject(true);
+    // $$.localPathRetrieverSync()
+  });
+
+  it("should check successfully", async () => {
+    const result = await MetadataCoverageCheck.run([]);
+    expect(result.success).to.equal(true);
+  });
+
+  it.skip("should fail when uncovered", async () => {
+    const result = await MetadataCoverageCheck.run([]);
+    expect(result.success).to.equal(false);
+  });
+});
